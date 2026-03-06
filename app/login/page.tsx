51f05456fece2400e337871f31ed7ce17ddc198d
naobot <naomi@nowme.ca>
2026-03-06 11:45:00 +0900

    feat: use Header on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,10 @@
+import Card from "@/components/ui/Card"
 import Header from "@/components/ui/Header"
 
 export default function LoginPage() {
   return (
-    <main>
+    <Card className="w-sm m-auto p-8px">
       <Header isLoggedIn={false} />
-    </main>
+    </Card>
   )
 }
