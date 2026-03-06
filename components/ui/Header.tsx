8ef8c2c0486cfcff923de18fbed41dea999afd0b
naobot <naomi@nowme.ca>
2026-03-06 11:46:04 +0900

    feat: link header logo to root

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,4 +1,5 @@
 // import styles from "./Card.module.css";
+import Link from "next/link";
 import { AuthButton } from "../auth/AuthButton";
 
 type HeaderProps = {
@@ -9,7 +10,11 @@ export default function Header({ isLoggedIn }: HeaderProps) {
   return (
     <div className="w-full flex justify-between items-center">
       <div>
-        <h1 className="text-sm uppercase text-pixel">Tomodex</h1>
+        <h1 className="text-sm uppercase text-pixel">
+          <Link href={"/"}>
+            Tomodex
+          </Link>
+        </h1>
       </div>
       <div>
         <AuthButton isLoggedIn />
